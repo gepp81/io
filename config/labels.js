fd24c5514aca2dbe46cdbd89cd4b380d6bcff5f3
gpidote <gpidote@cdt.com.ar>
2014-12-10 10:58:57 -0300

    Separacion de settings

---
+++ config/labels.js
@@ -0,0 +1,92 @@
+exports.labels = {
+  State: {
+    title: 'Provincia',
+    titleLower: 'provincia',
+    plural: 'provincias'
+  },
+  Sepulture: {
+    title: 'Sepultura',
+    titleLower: 'sepultura',
+    plural: 'sepulturas'
+  },
+  Acronym: {
+    title: 'Acrónimo',
+    titleLower: 'acrónimo',
+    plural: 'acrónimos'
+  },
+  Asocied: {
+    title: 'Asociado',
+    titleLower: 'asociado',
+    plural: 'asociados'
+  },
+  Sepulture: {
+    title: 'Sepultura',
+    titleLower: 'sepultura',
+    plural: 'sepulturas'
+  },
+  Acronym: {
+    title: 'Acrónimo',
+    titleLower: 'acrónimo',
+    plural: 'acrónimo'
+  },
+  Asocied: {
+    title: 'Asociado',
+    titleLower: 'asociado',
+    plural: 'asociados'
+  },
+  Conservation: {
+    title: 'Conservación',
+    titleLower: 'conservación',
+    plural: 'conservaciones'
+  },
+  Dating: {
+    title: 'Datación',
+    titleLower: 'datación',
+    plural: 'dataciones'
+  },
+  Age: {
+    title: 'Edad',
+    titleLower: 'edad',
+    plural: 'edades'
+  },
+  Incoming: {
+    title: 'Ingreso',
+    titleLower: 'ingreso',
+    plural: 'ingresos'
+  },
+  Integrity: {
+    title: 'Integridad',
+    titleLower: 'integridad',
+    plural: 'integridades'
+  },
+  Location: {
+    title: 'Locación',
+    titleLower: 'locación',
+    plural: 'locaciones'
+  },
+  OtherRest: {
+    title: 'Otros Restos',
+    titleLower: 'otros restos',
+    plural: 'otros restos'
+  },
+  Preservation: {
+    title: 'Preservación',
+    titleLower: 'preservación',
+    plural: 'preservaciones'
+  },
+  Accuracy: {
+    title: 'Precisión',
+    titleLower: 'presición',
+    plural: 'precisiones'
+  },
+  Sex: {
+    title: 'Sexo',
+    titleLower: 'sexo',
+    plural: 'sexos'
+  },
+  Site: {
+    title: 'Sitio',
+    titleLower: 'sitio',
+    plural: 'sitios'
+  }
+};
